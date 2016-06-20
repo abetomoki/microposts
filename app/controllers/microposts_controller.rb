51f9bcdbb147005875be7adc5a3611b13f30a3ee
TomokiAbe <catttm24@gmail.com>
2016-06-20 09:43:19 +0000

    add posting microposts

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -3,6 +3,14 @@ class MicropostsController < ApplicationController
     # もしもログインしていなければ、ログインページへ飛ばす。
     before_action :logged_in_user, only: [:create]
 
+    def destroy
+        @micropost = current_user.microposts.find_by(id: params[:id])
+        return redirect_to root_url if @micropost.nil?
+        @micropost.destroy
+        flash[:success] = "Micropost deleted"
+        redirect_to request.referrer || root_url
+    end
+
     def create
         # 現在のユーザに紐付いた@micropostに入れる？
         @micropost = current_user.microposts.build(micropost_params)
